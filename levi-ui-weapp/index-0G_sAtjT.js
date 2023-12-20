e0fd3ea690962e77860b71af1264e74ad15374f3
chan-levi <154293688+chan-levi@users.noreply.github.com>
2023-12-20 21:33:21 +0800

    Add files via upload
    reset init 2.0

---
+++ levi-ui-weapp/index-0G_sAtjT.js
@@ -0,0 +1,7 @@
+import{_ as c,o as n,c as r,a as e,u as o,b as d,d as s,e as t}from"./index-XncV1Wtc.js";const i={class:"container"},p={class:"carousel-section"},m={__name:"index",setup(u){const _=s(()=>t(()=>import("./common-header-gbRRLAeM.js"),__vite__mapDeps([0,1,2,3]))),a=s(()=>t(()=>import("./section-carousel-TfZYFjFS.js"),__vite__mapDeps([4,1,2,5])));return(l,f)=>(n(),r("div",i,[e(o(_)),d("div",p,[e(o(a))])]))}},x=c(m,[["__scopeId","data-v-bfeb1c23"]]);export{x as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["levi-ui-weapp/common-header-gbRRLAeM.js","levi-ui-weapp/index-XncV1Wtc.js","levi-ui-weapp/index-id6UF0Vn.css","levi-ui-weapp/common-header-qWX0bYjD.css","levi-ui-weapp/section-carousel-TfZYFjFS.js","levi-ui-weapp/section-carousel-NHiM-7Bw.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
