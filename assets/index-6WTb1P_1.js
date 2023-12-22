361e38e6a9e93a27610aab98bc09a2645a7c2cca
chan-levi <154293688+chan-levi@users.noreply.github.com>
2023-12-22 14:59:35 +0800

    Delete assets directory

--- assets/index-6WTb1P_1.js
+++
@@ -1,7 +0,0 @@
-import{_ as c,o as n,c as r,a as e,u as o,b as d,d as s,e as t}from"./index-1pR5XeWh.js";const i={class:"container"},p={class:"carousel-section"},m={__name:"index",setup(u){const _=s(()=>t(()=>import("./common-header-USGWwB4F.js"),__vite__mapDeps([0,1,2,3]))),a=s(()=>t(()=>import("./section-carousel-pP7FmPIH.js"),__vite__mapDeps([4,1,2,5])));return(l,f)=>(n(),r("div",i,[e(o(_)),d("div",p,[e(o(a))])]))}},x=c(m,[["__scopeId","data-v-bfeb1c23"]]);export{x as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["levi-ui-weapp/assets/common-header-USGWwB4F.js","levi-ui-weapp/assets/index-1pR5XeWh.js","levi-ui-weapp/assets/index-id6UF0Vn.css","levi-ui-weapp/assets/common-header-qWX0bYjD.css","levi-ui-weapp/assets/section-carousel-pP7FmPIH.js","levi-ui-weapp/assets/section-carousel-NHiM-7Bw.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
